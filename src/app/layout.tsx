b677bfbe28e3adac5a463177061a8f470eab6b0f
dettelink <deploy@local>
2026-05-25 19:18:13 +0500

    global css changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+
 
 export const metadata = {
   title: "Dettelinks - Corporate Finance Platform",
@@ -23,4 +23,8 @@ export default function RootLayout({
       <body>{children}</body>
     </html>
   );
+
+
+
+  
 }
